6f79dae750e1381336e805f8d882f6858a2404e1
Gavin Mendel-Gleason <gavin@scidonia.ai>
2025-12-28 00:17:44 +0100

    Adding finite digraph basics.

--- theories/Graph/FiniteDigraph.v
+++ theories/Graph/FiniteDigraph.v
@@ -1,22 +1,68 @@
 From Stdlib Require Import List Utf8.
-From stdpp Require Import base countable list gset.
+From stdpp Require Import prelude gmap.
 
 Import ListNotations.
 
 Set Default Proof Using "Type".
 
-Record fin_digraph (V : Type) `{EqDecision V} `{Countable V} := {
-  verts : gset V;
-  succ : V → list V;
-  succ_closed : ∀ v, v ∈ verts → Forall (λ w, w ∈ verts) (succ v);
-}.
+Section Digraph.
+  Context (V : Type) `{EqDecision V} `{Countable V}.
 
-Section Basic.
-  Context {V : Type} `{EqDecision V} `{Countable V}.
+  Record fin_digraph := {
+    verts : gset V;
+    succ : V → list V;
+    succ_closed : ∀ v, v ∈ verts → Forall (λ w, w ∈ verts) (succ v);
+  }.
 
-  Definition edge (G : fin_digraph V) (v w : V) : Prop :=
-    v ∈ G.(verts) ∧ w ∈ G.(succ) v.
+  Definition edge (G : fin_digraph) (v w : V) : Prop :=
+    v ∈ verts G ∧ w ∈ succ G v.
 
-  Definition out_neighbours (G : fin_digraph V) (v : V) : list V :=
-    G.(succ) v.
-End Basic.
+  Definition out_neighbours (G : fin_digraph) (v : V) : list V :=
+    succ G v.
+
+  Fixpoint last_error (xs : list V) : option V :=
+    match xs with
+    | [] => None
+    | [x] => Some x
+    | _ :: xs' => last_error xs'
+    end.
+
+  Fixpoint edges_from (G : fin_digraph) (v : V) (xs : list V) : Prop :=
+    match xs with
+    | [] => True
+    | w :: xs' => w ∈ succ G v ∧ edges_from G w xs'
+    end.
+
+  Definition edges_along (G : fin_digraph) (xs : list V) : Prop :=
+    match xs with
+    | [] => True
+    | v :: xs' => edges_from G v xs'
+    end.
+
+  Definition is_path (G : fin_digraph) (xs : list V) : Prop :=
+    Forall (λ v, v ∈ verts G) xs ∧ edges_along G xs.
+
+  Definition is_path_from (G : fin_digraph) (v : V) (xs : list V) : Prop :=
+    hd_error xs = Some v ∧ is_path G xs.
+
+  Definition is_path_to (G : fin_digraph) (v : V) (xs : list V) : Prop :=
+    last_error xs = Some v ∧ is_path G xs.
+
+  Definition is_cycle (G : fin_digraph) (xs : list V) : Prop :=
+    ∃ v ys,
+      xs = v :: ys ++ [v] ∧
+      ys ≠ [] ∧
+      is_path G xs.
+
+  Lemma edge_iff (G : fin_digraph) (v w : V) :
+    edge G v w ↔ v ∈ verts G ∧ w ∈ succ G v.
+  Proof.
+    unfold edge.
+    tauto.
+  Qed.
+End Digraph.
+
+Arguments fin_digraph {_} {_} {_}.
+Arguments verts {_} {_} {_} _.
+Arguments succ {_} {_} {_} _ _.
+Arguments succ_closed {_} {_} {_} _ _ _.
--- theories/Preproof/Preproof.v
+++ theories/Preproof/Preproof.v
@@ -1,7 +1,7 @@
-From Coq Require Import List.
-From stdpp Require Import base list gset.
+From Stdlib Require Import List Utf8.
+From stdpp Require Import prelude countable gmap.
 
-From cyclic.theories.Graph Require Import FiniteDigraph.
+From Cyclic.Graph Require Import FiniteDigraph.
 
 Import ListNotations.
 
@@ -9,21 +9,21 @@ Set Default Proof Using "Type".
 
 Section PreProof.
   Context {Judgement : Type}.
-  Context (Rule : Judgement -> list Judgement -> Prop).
+  Context (Rule : Judgement → list Judgement → Prop).
 
   Context {V : Type} `{EqDecision V} `{Countable V}.
 
   Record preproof := {
-    pp_graph : fin_digraph V;
-    pp_label : V -> Judgement;
-    pp_rule_ok : forall v,
-      v ∈ pp_graph.(verts) ->
-      Rule (pp_label v) (map pp_label (pp_graph.(succ) v));
+    pp_graph : fin_digraph;
+    pp_label : V → Judgement;
+    pp_rule_ok : ∀ v,
+      v ∈ verts pp_graph →
+      Rule (pp_label v) (map pp_label (succ pp_graph v));
   }.
 
   Record rooted_preproof := {
     rpp_proof : preproof;
     rpp_root : V;
-    rpp_root_in : rpp_root ∈ rpp_proof.(pp_graph).(verts);
+    rpp_root_in : rpp_root ∈ verts rpp_proof.(pp_graph);
   }.
 End PreProof.
