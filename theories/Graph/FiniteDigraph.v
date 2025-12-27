aa6d7d0ca9e2afae746076b238b7027b552552a9
Gavin Mendel-Gleason <gavin@scidonia.ai>
2025-12-27 23:07:07 +0100

    Better syntax

--- theories/Graph/FiniteDigraph.v
+++ theories/Graph/FiniteDigraph.v
@@ -1,5 +1,5 @@
-From Coq Require Import List.
-From stdpp Require Import base list gmap gset.
+From Stdlib Require Import List Utf8.
+From stdpp Require Import base countable list gset.
 
 Import ListNotations.
 
@@ -7,15 +7,15 @@ Set Default Proof Using "Type".
 
 Record fin_digraph (V : Type) `{EqDecision V} `{Countable V} := {
   verts : gset V;
-  succ : V -> list V;
-  succ_closed : forall v, v ∈ verts -> Forall (fun w => w ∈ verts) (succ v);
+  succ : V → list V;
+  succ_closed : ∀ v, v ∈ verts → Forall (λ w, w ∈ verts) (succ v);
 }.
 
 Section Basic.
   Context {V : Type} `{EqDecision V} `{Countable V}.
 
   Definition edge (G : fin_digraph V) (v w : V) : Prop :=
-    v ∈ G.(verts) /\ w ∈ G.(succ) v.
+    v ∈ G.(verts) ∧ w ∈ G.(succ) v.
 
   Definition out_neighbours (G : fin_digraph V) (v : V) : list V :=
     G.(succ) v.
